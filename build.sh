252da48125781c104f9ec1fa4fbaa92915e70c7e
Andrew Innes <andrew.c12@gmail.com>
2016-04-16 10:12:24 +0800

    Remove all automatic provisioning due to lack of space

--- build.sh
+++ build.sh
@@ -34,18 +34,19 @@ IMGBUILDERURL="https://downloads.openwrt.org/chaos_calmer/${RELEASE}/ar71xx/gene
 # the absolute minimum for extroot to work at all (i.e. when the disk is already set up, for example by hand).
 # this list may be smaller and/or different for your router, but it works with my ar71xx.
 PREINSTALLED_PACKAGES="block-mount kmod-usb2 kmod-usb-storage kmod-fs-ext4"
+PREINSTALLED_PACKAGES+=" blkid"
 
 # some kernel modules may also be needed for your hardware
-#PREINSTALLED_PACKAGES+=" kmod-usb-uhci kmod-usb-ohci"
+PREINSTALLED_PACKAGES+=" kmod-usb-uhci kmod-usb-ohci"
 
 # these are needed for the proper functioning of the auto extroot scripts
-PREINSTALLED_PACKAGES+=" blkid mount-utils swap-utils e2fsprogs fdisk"
+#PREINSTALLED_PACKAGES+=" mount-utils swap-utils e2fsprogs fdisk"
 
 # the following packages are optional, feel free to (un)comment them
-PREINSTALLED_PACKAGES+=" wireless-tools firewall iptables"
-PREINSTALLED_PACKAGES+=" kmod-usb-storage-extras kmod-mmc"
-PREINSTALLED_PACKAGES+=" ppp ppp-mod-pppoe ppp-mod-pppol2tp ppp-mod-pptp kmod-ppp kmod-pppoe"
-PREINSTALLED_PACKAGES+=" luci"
+#PREINSTALLED_PACKAGES+=" wireless-tools firewall iptables"
+#PREINSTALLED_PACKAGES+=" kmod-usb-storage-extras kmod-mmc"
+#PREINSTALLED_PACKAGES+=" ppp ppp-mod-pppoe ppp-mod-pppol2tp ppp-mod-pptp kmod-ppp kmod-pppoe"
+#PREINSTALLED_PACKAGES+=" luci"
 
 mkdir --parents ${BUILD}
 
